a5a730c3c34bcf2e1bdce6802ceaef00b83d6820
Geno Sanchez <39529787+genoes@users.noreply.github.com>
2023-03-07 19:39:31 -0800

    Update combine-csv-cat.py

--- combine-csv-cat/combine-csv-cat.py
+++ combine-csv-cat/combine-csv-cat.py
@@ -1,26 +1,23 @@
 import pandas as pd
 import glob
 
-# Define the directory where the CSV files are located
+# Define variables
 path = input('\n'"Enter absolute path to CSV directory: "'\n').strip()
 column_header = input('\n'"Enter column header you wish to concatenate by (e.g. Object identifier): "'\n').strip()
 
-# Get a list of all the CSV files in the directory
 all_files = glob.glob(path + "/*.csv")
 
-# Initialize an empty list to store the DataFrames for each file
 li = []
 
-# Loop through each file and read it into a DataFrame
 for filename in all_files:
     df = pd.read_csv(filename, index_col=None, header=0)
     li.append(df)
 
 
-# Combine the DataFrames, matching columns and filling missing values with NaN
+# Combine the dfs, matching columns and filling missing values with NaN
 combined_df = pd.concat(li, axis=0, ignore_index=True, sort=False)
 
-# Group the rows by 'Object identifier' and concatenate the cells in each column
+# Group the rows by column_header and concatenate the cells in each column
 grouped_df = combined_df.groupby(column_header).agg(lambda x: ' | '.join(filter(lambda y: pd.notna(y) and str(y).strip() != '', set(x.astype(str).str.strip().values))))
 
 # Delete all NaN values
@@ -32,7 +29,7 @@ grouped_df = grouped_df.reset_index()
 # Reorder the columns alphabetically
 #grouped_df = grouped_df.reindex(sorted(grouped_df.columns), axis=1)
 
-# Write the combined DataFrame to a new CSV file
+# Write combined df to a new CSV file
 grouped_df.to_csv("combined_sheet.csv", index=False, encoding = 'utf-8')
 print('\n''''
   ___                 ___  
